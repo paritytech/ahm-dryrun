c30ebd1946153dd63507751fe90c1f9555469e6f
ndk <ndk@parity.io>
2025-03-17 12:15:32 +0100

    added migration resuming from the highest block

--- index.ts
+++ index.ts
@@ -2,22 +2,30 @@ import { test } from "bun:test";
 import { setupNetworks } from '@acala-network/chopsticks-testing'
 
 // run it with bun test ./index.ts --timeout=22000000
+// 22000000 miliseconds ~ 6.1 hours which should be enough (in theory) to run accounts migration
 test('test migration run', async() => {
     const {polkadot, assetHub} = await setupNetworks({
         polkadot: {
             endpoint: 'wss://polkadot.rpc.permanence.io',
-            block: 25100182,
+            // resumes from the highest block.
+            resume: true,
+            // setupNetworks invokes dev.newBlock to override the wasm, so blocks in the db start from block+1
+            // block: 25172409, -- initial block
             'wasm-override': 'runtime_wasm/polkadot_runtime.compact.compressed.wasm',
-            'runtime-log-level': 5,
+            // 'runtime-log-level': 5,
             'fetch-storages': '0x', // universal prefix
             db: './rc-db.sqlite',
             port: 8000,
         },
         assetHub: {
             endpoint: 'wss://polkadot-asset-hub-rpc.polkadot.io',
+            // resumes from the highest block.
+            resume: true,
+            // setupNetworks invokes dev.newBlock to override the wasm, so blocks in the db start from block+1
+            // block: 8433079, -- initial block
             'wasm-override': 'runtime_wasm/asset_hub_polkadot_runtime.compact.compressed.wasm',
             'fetch-storages': '0x',
-            'runtime-log-level': 5,
+            // 'runtime-log-level': 5,
             db: './ah-db.sqlite',
             port: 8001,
         },
@@ -33,4 +41,6 @@ test('test migration run', async() => {
         console.log('new RC migration stage is ', migrationStage);
         stageName = migrationStage ? Object.keys(migrationStage)[0] : null;
     }
+
+    console.log('migration has finished');
 });
