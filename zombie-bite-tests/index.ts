477633adc1ce13609de31df016e258535061b8b8
ndk <ndk@parity.io>
2025-04-03 18:45:27 +0200

    added a script to sudo call runtime upgrade on Asset Hub

--- zombie-bite-tests/index.ts
+++ zombie-bite-tests/index.ts
@@ -1,30 +1,26 @@
-// import { test, expect } from "bun:test";
-//
-// import { polkadot_rc } from "@polkadot-api/descriptors";
-//
-// import { Binary, Enum, createClient } from "polkadot-api";
-// import { getWsProvider } from "polkadot-api/ws-provider/web";
-// import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
-//
-// import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
-//
-//
-// const client = createClient(withPolkadotSdkCompat(getWsProvider("ws://localhost:59015")));
-// const RCApi = client.getTypedApi(polkadot_rc);
-//
-
-
-
 import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
 import {
-    DEV_PHRASE,
-    entropyToMiniSecret,
-    mnemonicToEntropy,
+  DEV_PHRASE,
+  entropyToMiniSecret,
+  mnemonicToEntropy,
 } from "@polkadot-labs/hdkd-helpers";
 
+import {
+  polkadot_rc,
+  XcmV4Instruction,
+  XcmV3WeightLimit,
+  XcmV3Junctions,
+  XcmV3Junction,
+  XcmVersionedLocation,
+  XcmVersionedXcm,
+  XcmV2OriginKind,
+} from "@polkadot-api/descriptors";
+
 import { getPolkadotSigner } from "polkadot-api/signer";
-//
-//
+import { Binary, createClient } from "polkadot-api";
+import { getWsProvider } from "polkadot-api/ws-provider/web";
+import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
+
 // Initialize HDKD key pairs and signers
 const entropy = mnemonicToEntropy(DEV_PHRASE);
 const miniSecret = entropyToMiniSecret(entropy);
@@ -33,47 +29,47 @@ const derive = sr25519CreateDerive(miniSecret);
 const hdkdKeyPairAlice = derive("//Alice");
 
 const aliceSigner = getPolkadotSigner(
-    hdkdKeyPairAlice.publicKey,
-    "Sr25519",
-    hdkdKeyPairAlice.sign,
+  hdkdKeyPairAlice.publicKey,
+  "Sr25519",
+  hdkdKeyPairAlice.sign,
 );
 
-import { polkadot_rc, XcmV4Instruction, XcmV3WeightLimit } from "@polkadot-api/descriptors";
-
-import { Binary, Enum, createClient } from "polkadot-api";
-import { getWsProvider } from "polkadot-api/ws-provider/web";
-import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
-
-import { ApiPromise, WsProvider, Keyring }  from '@polkadot/api'
-
-const client = createClient(withPolkadotSdkCompat(getWsProvider("ws://localhost:54568")));
+const client = createClient(
+  withPolkadotSdkCompat(getWsProvider("ws://localhost:63168")),
+);
 const RCApi = client.getTypedApi(polkadot_rc);
 
-const provider = new WsProvider('ws://127.0.0.1:54568');
-const api = await ApiPromise.create({ provider });
+const dest = XcmVersionedLocation.V4({
+  parents: 1,
+  interior: XcmV3Junctions.X1(XcmV3Junction.Parachain(1000)),
+});
 
-const keyring = new Keyring({ type: 'sr25519' });
-const alice = keyring.addFromUri('//Alice');
-
-console.log(`Alice's Address: ${alice.address}`);
-
-const balance = await api.query.system.account(alice.address);
-console.log(`Alice's Balance: ${balance}`);
-
-
-// const msg = Enum("V4", [XcmV4Instruction.ClearOrigin()]);
-const msg = Enum("V4", [XcmV4Instruction.UnpaidExecution({
+const message = XcmVersionedXcm.V4([
+  XcmV4Instruction.UnpaidExecution({
     weight_limit: XcmV3WeightLimit.Unlimited(),
-})]);
-const res = await RCApi.tx.XcmPallet.execute({
-    message: msg,
-    max_weight: {ref_time: 999999999n, proof_size: 7777777n },
-}).signAndSubmit(aliceSigner);
+    check_origin: undefined,
+  }),
+  XcmV4Instruction.Transact({
+    origin_kind: XcmV2OriginKind.Superuser(),
+    require_weight_at_most: {
+      ref_time: 999999999n,
+      proof_size: 7777777n,
+    },
+    call: Binary.fromHex(
+      "0x00090b4504a75e80db9cc956610c2a7ca5689df83a15a5311a1d62e4e3bf7bd2825c",
+    ),
+  }),
+]);
 
-console.log("res is ", res);
+const xcmCall = RCApi.tx.XcmPallet.send({
+  dest: dest,
+  message: message,
+});
 
-// const res = await RCApi.tx.xcmPallet.send({parents: 1, interior: XcmV3Junctions.X1(XcmV3Junction.Parachain(1000))}, {V4: [ {
-//         SetFeesMode: {
-//             jit_withdraw: true,
-//         },
-//     } ]}).signAndSubmit(aliceSigner);
+const sudoCall = RCApi.tx.Sudo.sudo({
+  call: xcmCall.decodedCall,
+});
+
+const hash = await sudoCall.signAndSubmit(aliceSigner);
+
+console.log(`Transaction sent! Hash: ${hash}`);
