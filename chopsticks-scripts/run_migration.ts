d68342d3db07d9a2d8bf518326c1645c7e0dda37
ndk <ndk@parity.io>
2025-04-18 19:42:00 +0200

    a script to run AH migration

--- chopsticks-scripts/run_migration.ts
+++ chopsticks-scripts/run_migration.ts
@@ -1,85 +1,70 @@
+
+
 import { setupNetworks } from "@acala-network/chopsticks-testing";
 import * as dotenv from 'dotenv';
+import { fetchStorages } from '@acala-network/chopsticks/utils/fetch-storages'
 
 dotenv.config();
 const { polkadot, assetHub } = await setupNetworks({
     polkadot: {
-        endpoint: process.env.POLKADOT_ENDPOINT || `ws://localhost:${process.env.RELAY_NODE_RPC_PORT}`,
-        // endpoint: "wss://polkadot.rpc.permanence.io",
-        // resumes from the highest block.
-        // resume: true,
-        // setupNetworks invokes dev.newBlock to override the wasm, so blocks in the db start from block+1
-        block: process.env.POLKADOT_BLOCK_NUMBER_PRE,
+        endpoint: `ws://localhost:${process.env.RELAY_NODE_RPC_PORT}`,
         "wasm-override": "runtime_wasm/polkadot_runtime.compact.compressed.wasm",
-        // 'runtime-log-level': 5,
-        // "prefetch-storages": ["0x"], // universal prefix
         db: "./dbs/polkadot.sqlite",
         port: 8000,
     },
     assetHub: {
-        endpoint:
-            process.env.POLKADOT_ASSET_HUB_ENDPOINT || `ws://localhost:${process.env.AH_NODE_RPC_PORT}`,
-            // "wss://polkadot-asset-hub-rpc.polkadot.io",
-        // resumes from the highest block.
-        // resume: true,
-        // setupNetworks invokes dev.newBlock to override the wasm, so blocks in the db start from block+1
-        block: process.env.POLKADOT_ASSET_HUB_BLOCK_NUMBER_PRE,
+        endpoint: `ws://localhost:${process.env.AH_NODE_RPC_PORT}`,
         "wasm-override":
             "runtime_wasm/asset_hub_polkadot_runtime.compact.compressed.wasm",
-        // "prefetch-storages": ["0x"],
-        // 'runtime-log-level': 5,
         db: "./dbs/polkadot-asset-hub.sqlite",
         port: 8001,
     },
 });
 
-let rcAccBefore = (
-    await polkadot.api.query.system.account(
-        "5Ee7mSUN7p9YGqzthB1uCbQPMo9zC2Z2Yv5b2nsHKDzmtseR",
-    )
-).data;
-let ahAccBefore = (
-    await assetHub.api.query.system.account(
-        "5Ee7mSUN7p9YGqzthB1uCbQPMo9zC2Z2Yv5b2nsHKDzmtseR",
-    )
-).data;
-console.log("rcAccBefore: ", rcAccBefore.toHuman());
-console.log("ahAccBefore: ", ahAccBefore.toHuman());
+await fetchStorages({
+   // block: process.env.POLKADOT_BLOCK_NUMBER_PRE,
+   endpoint: `ws://localhost:${process.env.RELAY_NODE_RPC_PORT}`,
+   dbPath: "./dbs/polkadot.sqlite",
+   config: ['System', 'Balances'],
+});
+console.log('relay chain storage fetched');
 
-let ahMigrationStage = (
-    await assetHub.api.query.ahMigrator.ahMigrationStage()
-).toHuman();
-let rcMigrationStage = (
-    await polkadot.api.query.rcMigrator.rcMigrationStage()
-).toHuman();
+await fetchStorages({
+   // block: process.env.POLKADOT_ASSET_HUB_BLOCK_NUMBER_PRE,
+   endpoint: `ws://localhost:${process.env.AH_NODE_RPC_PORT}`,
+   dbPath: "./dbs/polkadot-asset-hub.sqlite",
+   config: ['0x'],
+});
+console.log('asset hub storage fetched');
+
+
+await polkadot.dev.setStorage({
+    rcMigrator: {
+        rcMigrationStage: 'AccountsMigrationInit',
+    },
+})
+
+await assetHub.dev.setStorage({
+    ahMigrator: {
+        ahMigrationStage: 'DataMigrationOngoing',
+    },
+});
+
+await polkadot.chain.newBlock(); // Init -> Ongoing
+await assetHub.chain.newBlock();
+
+
+let rcMigrationStage = (await polkadot.api.query.rcMigrator.rcMigrationStage()).toHuman();
 let rcStageName = rcMigrationStage ? Object.keys(rcMigrationStage)[0] : null;
 
-for (let i = 0; i < 3; i++) {
-    await polkadot.dev.newBlock();
+console.log('rcMigrationStage before: ', rcMigrationStage);
+while (rcStageName == 'AccountsMigrationOngoing') {
+    await polkadot.chain.newBlock();
+    await assetHub.chain.newBlock();
 
-    ahMigrationStage = (
-        await assetHub.api.query.ahMigrator.ahMigrationStage()
-    ).toHuman();
-    // console.log('new AH migration stage is ', ahMigrationStage);
-
-    rcMigrationStage = (
-        await polkadot.api.query.rcMigrator.rcMigrationStage()
-    ).toHuman();
-    // console.log('new RC migration stage is ', rcMigrationStage);
+    rcMigrationStage = (await polkadot.api.query.rcMigrator.rcMigrationStage()).toHuman();
     rcStageName = rcMigrationStage ? Object.keys(rcMigrationStage)[0] : null;
+    console.log('rcMigrationStage: ', rcMigrationStage);
+    // asset hub's stage stays the same, no need to verify.
 }
-
-console.log("migration has finished");
-
-let rcAccAfter = (
-    await polkadot.api.query.system.account(
-        "5Ee7mSUN7p9YGqzthB1uCbQPMo9zC2Z2Yv5b2nsHKDzmtseR",
-    )
-).data;
-let ahAccAfter = (
-    await assetHub.api.query.system.account(
-        "5Ee7mSUN7p9YGqzthB1uCbQPMo9zC2Z2Yv5b2nsHKDzmtseR",
-    )
-).data;
-console.log("rcAccAfter: ", rcAccAfter.toHuman());
-console.log("ahAccAfter: ", ahAccAfter.toHuman());
+console.log('migration has finished')
