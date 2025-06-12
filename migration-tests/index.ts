e1b84d5c5311e673ea3f8ca1d11ea80aff4dc902
Javier Viola <javier@parity.io>
2025-06-12 12:59:34 +0200

    fmt

--- migration-tests/index.ts
+++ migration-tests/index.ts
@@ -1,17 +1,17 @@
 import { main, ChainConfig } from "./lib.js";
 import fs from "fs";
 
-let rc_chain_config: ChainConfig|undefined;
-let ah_chain_config: ChainConfig|undefined;
-if(process.argv[2]) {
-    rc_chain_config = JSON.parse(fs.readFileSync(process.argv[2], 'utf-8'));
+let rc_chain_config: ChainConfig | undefined;
+let ah_chain_config: ChainConfig | undefined;
+if (process.argv[2]) {
+  rc_chain_config = JSON.parse(fs.readFileSync(process.argv[2], "utf-8"));
 }
 
-if(process.argv[3]) {
-    ah_chain_config = JSON.parse(fs.readFileSync(process.argv[3], 'utf-8'));
+if (process.argv[3]) {
+  ah_chain_config = JSON.parse(fs.readFileSync(process.argv[3], "utf-8"));
 }
 
 main(rc_chain_config, ah_chain_config).catch((error) => {
-    console.error(error);
-    process.exit(1);
+  console.error(error);
+  process.exit(1);
 });
