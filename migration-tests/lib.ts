5207582b6b5194e1385098c0c5d218eb90ae8e93
ndk <ndk@parity.io>
2025-06-24 16:30:04 +0200

    fixed merge conflict

--- migration-tests/lib.ts
+++ migration-tests/lib.ts
@@ -5,12 +5,17 @@ import { ApiPromise, WsProvider } from "@polkadot/api";
 import { MigrationTest, TestContext } from "./types.js";
 import { vestingTests } from "./pallets/vesting.js";
 import { assetRateTests } from './pallets/asset_rate.js';
+import { voterListTests } from './pallets/staking/voter_list.js';
+import { convictionVotingTests } from "./pallets/conviction_voting.js";
+
 // import { bountiesTests } from './pallets/bounties.js';
 
 export const tests: MigrationTest[] = [
   // bountiesTests,
   vestingTests,
-  assetRateTests
+  assetRateTests,
+  voterListTests,
+  convictionVotingTests
 ];
 
 export async function runTests(context: TestContext) {
