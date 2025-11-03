aca5570ca92c4e6813feecaec3962ff53b6114fb
ndk <ndk@parity.io>
2025-11-03 12:43:48 +0100

    now script finds exact Kusama AH block and its corresponding RC block

--- zombie-bite-scripts/first_block_in_next_era.ts
+++ zombie-bite-scripts/first_block_in_next_era.ts
@@ -1,8 +1,5 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { assert } from '@polkadot/util';
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
 
 interface EventRecord {
     blockNumber: number;
@@ -11,96 +8,55 @@ interface EventRecord {
     data: any;
 }
 
-async function findFirstBlockInNextEra(originalApi: ApiPromise, postAHMAPI: ApiPromise): Promise<number> {
-  // Get current active era
-  const activeEra = await postAHMAPI.query.staking.activeEra();
-  if (activeEra.isNone) {
-    throw new Error('No active era found');
-  }
-
-  const nextEraIndex = activeEra.unwrap().index.toNumber() + 1;
-
+async function findEraStartBlock(originalApi: ApiPromise, postAHMAPI: ApiPromise): Promise<[number, number]> {
   const currentBlock = await postAHMAPI.query.ahMigrator.migrationEndBlock();
-  const currentBlockNumber = Number(currentBlock);
+  const currentBlockNumber = 11152290; // closer to era change (11152292) then actual migration end block
+//   const currentBlockNumber = Number(currentBlock);
 
   const allEvents: EventRecord[] = [];
-  let lastBlock = currentBlockNumber + 8000; //14400; // Look forward for max 24*60*60/6 blocks (24 hours / 6 seconds per block)
-  
-  // Prepare CSV file if it's the first block
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const csvFilePath = path.join(__dirname, 'events.csv');
-  if (!fs.existsSync(csvFilePath)) {
-      fs.writeFileSync(csvFilePath, 'blockNumber,section,method,data\n', { encoding: 'utf-8' });
-  }
+  // Look forward for max 24*60*60/6 blocks (24 hours / 6 seconds per block)
+  let lastBlock = currentBlockNumber + 14400;
 
-  const currentEraOpt = await postAHMAPI.query.staking.currentEra();
-  const currentEra = currentEraOpt.isSome ? currentEraOpt.unwrap().toNumber() : -1;
-  
-
-  for (let i = lastBlock; i >= currentBlockNumber; i--) {
-    if (i % 180 === 0) {
+  for (let i = currentBlockNumber + 1; i <= lastBlock; i++) {
+    if (i % 200 === 0) {
       console.log(`Processing block ${i}`);
     }
+
     const blockHash = await originalApi.rpc.chain.getBlockHash(i);
     const apiAtBlock = await originalApi.at(blockHash);
-    const systemEvents = await apiAtBlock.query.system.events();
 
+    const currentEraOpt = await apiAtBlock.query.staking.currentEra();
+    const currentEra = currentEraOpt.isSome ? currentEraOpt.unwrap().toNumber() : -1;
+    
+    const systemEvents = await apiAtBlock.query.system.events();
     for (const event of systemEvents) {
         const e = {
             blockNumber: i,
             section: event.event.section,
             method: event.event.method,
-            data: JSON.stringify(event.event.data.toJSON()).replace(/"/g, '""'), // escape quotes for CSV
+            data: JSON.stringify(event.event.data.toJSON()),
         };
         allEvents.push(e);
 
-        // Write to CSV
-        const csvLine = `${e.blockNumber},"${e.section}","${e.method}","${e.data}"\n`;
-        fs.appendFileSync(csvFilePath, csvLine, { encoding: 'utf-8' });
+        if (e.section.toLowerCase() === "staking" && e.method.toLowerCase() === "sessionrotated") {
+            const data = event.event.data.toJSON() as any[];
+            const [startingSession, activeEra, planned] = data;
 
-        if (event.event.section.toLowerCase() === "staking" && event.event.method.toLowerCase() === "sessionrotated") {
-            // Still optionally log the block where the session is rotated
-            console.log(`SessionRotated event found at block ${i}`);
-            const data = event.event.data.toJSON() as any;
-            const activeEra = data.activeEra;
-            const plannedEra = data.plannedEra;
-            if (activeEra === currentEra - 1 && plannedEra === currentEra) {
-                console.log(`Found a block in the next era: ${i}`);
-                return i;
+            if (activeEra === currentEra-1 && planned === currentEra) {
+                const relayBlockNumber = await apiAtBlock.query.parachainSystem.lastRelayChainBlockNumber();
+                const rcBlockNumber = Number(relayBlockNumber.toPrimitive());
+                
+                return [rcBlockNumber, i];
             }
-            // return i;
         }
     }
   }
-  throw new Error(`Could not find first block in era ${nextEraIndex} within search range ${currentBlockNumber} to ${lastBlock}`);
-}
-
-async function verifyPreviousBlockInPreviousEra(api: ApiPromise, block: number) {
-    const prevBlockHash = await api.rpc.chain.getBlockHash(block - 1);
-    const prevApiAtBlock = await api.at(prevBlockHash);
-    const prevEraAtBlock = await prevApiAtBlock.query.staking.activeEra();
-
-    if (prevEraAtBlock.isNone) {
-      throw new Error(`Previous block ${block - 1} has no era information`);
-    }
-    const prevEraIndex = prevEraAtBlock.unwrap().index.toNumber();
-
-    const currentBlockhash = await api.rpc.chain.getBlockHash(block);
-    const currentApiAtBlock = await api.at(currentBlockhash);
-    const currentEraAtBlock = await currentApiAtBlock.query.staking.activeEra();
-
-    if (currentEraAtBlock.isNone) {
-      throw new Error(`Current block ${block} has no era information`);
-    }
-    const currentEraIndex = currentEraAtBlock.unwrap().index.toNumber();
-
-    assert(prevEraIndex === currentEraIndex - 1, 
-      `Previous block ${block - 1} is in era ${prevEraIndex}, expected to be in era ${currentEraIndex - 1}`);
+  throw new Error(`Could not find currentEra === activeEra + 1 within search range ${currentBlockNumber} to ${lastBlock}`);
 }
 
 async function main(network: string) {
     const endpoints: Record<string, string> = {
-      // TODO: make a list of endpoints and retry connection if failed
+      // TODO: add Polkadot endpoints and retry connection if failed
       'kusama': 'wss://kusama-asset-hub-rpc.polkadot.io',
     };
   
@@ -120,14 +76,9 @@ async function main(network: string) {
         const postAHMBlockNumber = Number(migrationEndBlock.toPrimitive());
         const postAHMBlockHash = await api.rpc.chain.getBlockHash(postAHMBlockNumber);
         const postAHMAPI = await api.at(postAHMBlockHash);
-        // migrationEndBlockNumber = migrationEndBlockNumber + 1;
-        const block = await findFirstBlockInNextEra(api, postAHMAPI as ApiPromise);
-        console.log(`First block in the next era: ${block}`);
-        await verifyPreviousBlockInPreviousEra(api, block);
-  
-        const rcBiteBlock = block;
-        console.log(rcBiteBlock);
-  
+        
+        const [rcBlock, ahBlock] = await findEraStartBlock(api, postAHMAPI as ApiPromise);
+        console.log(`✅ First block in the next era - RC: ${rcBlock}, AH: ${ahBlock}`);
     } catch (error) {
       console.error('Error:', error);
       process.exit(1);
